0dd861d5b496d5d4abc3331c5a47d1fa5f9dea08
Chris Wong <wc38.wong@gmail.com>
2021-07-19 12:04:52 +0800

    AntiForgery.Validate

--- VueWebApplication/ApiControllers/StudentController.cs
+++ VueWebApplication/ApiControllers/StudentController.cs
@@ -1,40 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
+using System.Web.Helpers;
 using System.Web.Http;
 
 namespace VueWebApplication.ApiControllers
 {
     public class StudentController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        /// <summary>
+        /// Validate the token
+        /// </summary>
+        /// <param name="request"></param>
+        public static void ValidateRequestHeader(HttpRequestMessage request)
         {
-            return new string[] { "student1", "student2" };
-        }
+            string cookieToken = "";
+            string formToken = "";
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
+            if (request.Headers.TryGetValues("RequestVerificationToken", out IEnumerable<string> tokenHeaders))
+            {
+                cookieToken = request.Headers.GetCookies("__RequestVerificationToken")[0].ToString().Replace("__RequestVerificationToken=", "");
+                formToken = tokenHeaders.First();
+            }
+            AntiForgery.Validate(cookieToken, formToken);
         }
 
         // POST api/<controller>
-        public IEnumerable<string> Post([FromBody]string value)
+        public IEnumerable<string> Post(HttpRequestMessage request)
         {
+            ValidateRequestHeader(request);
             return new string[] { "student1", "student2", "student3" };
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
         }
     }
 }
